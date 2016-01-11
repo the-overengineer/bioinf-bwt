65bbbd614bb78bb596c46f2b677bf921f1648cf3
Matija Herceg <herc.rules@gmail.com>
2016-01-11 18:10:39 +0000

    Added short delete test

--- test.cpp
+++ test.cpp
@@ -42,17 +42,7 @@ dynsa::DynamicSuffixArray *a;
 
 int main( int argc, char* const argv[]){
     
-    dynsa::ustring s = new uchar[10];
-    s[0] = (uchar) 'G';
-    s[1] = (uchar) 'T';
-    s[2] = (uchar) 'G';
-    s[3] = (uchar) 'A';
-    s[4] = (uchar) 'G';
-    s[5] = (uchar) 'T';
-    s[6] = (uchar) 'G';
-    s[7] = (uchar) 'A';
-    s[8] = (uchar) 'G';
-    s[9] = (uchar) 0;
+    ustring s = (ustring) "CCGAACACCCGTACCGAGGTGAGCACCCTCTGCATCAGCCAGTTCCGCTACAGCGCACAAATCCGTCCTTCTTCCGTAGTGACCAAAGACTACACCTTTAAACGCCCCGGCTGGGCCGGACGTTTTGAACAGGAAGGCCAGCACCAGGACTACCAGCGCACGCAGTATGAAGTGTATGACTACCCCGGACGTTTCAAGAGCGCCCACGGGCAGAACTTTGCCCGCTGGCAGATGGACGGCTGGCGAAACAACGCAGAAACCGCGCGGGGAATGAGCCGCTCGCCGGAGATATGGCCGGGACGGCGAATTGTGCTGACGGGGCATCCGCAGGCGAACCTGAACCGGGAATGGCAGGTGGTGGCAAGTGAACTGCACGGCGAACAGCCACAGGCGGTGCCAGGACGGCAGGGAGCGGGGACGGCGCTGGAGAACCATTTTGCGGTGATCCCGGCAGACAGAACATGGCGACCACAGCCGTTGCTGAAACCGCTGGTCGACGGCCCGCAGAGCGC";
 
     float* fs = DynRankS::createCharDistribution(s, 10, 1);
     a = new dynsa::DynamicSuffixArray(fs);
@@ -81,6 +71,17 @@ TEST_CASE("Short insert", "[short]"){
     ustring bwt = (ustring) "GGGGAATTT$AAGGG";
     int sz = sizeof(s);
     
+    CHECK( memcmp(a->getText(), s, sz) == 0);
+    sz = sizeof(bwt);
+    REQUIRE(memcmp(a->getBWT(), bwt, sz) == 0);
+}
+
+TEST_CASE("Short delete", "[short]"){
+    ustring s = (ustring) "GTGAGTGAG";
+    ustring bwt = (ustring) "GGGATTA$GG";
+    a -> deleteAt(3,5);
+    
+    int sz = sizeof(s);
     CHECK( memcmp(a->getText(), s, sz) == 0);
     sz = sizeof(bwt);
     REQUIRE(memcmp(a->getBWT(), bwt, sz) == 0);
