fd13bb25ddeb80e4ddf58403ac032a928e5b6e3a
Matija Herceg <herc.rules@gmail.com>
2016-01-14 20:13:55 +0000

    Code cleanup

--- test.cpp
+++ test.cpp
@@ -7,36 +7,6 @@
 
 using namespace std;
 
-/*int main() {
-    dynsa::ustring s = new uchar[5];
-    s[0] = (uchar) 'G';
-    s[1] = (uchar) 'T';
-    s[2] = (uchar) 'G';
-    s[3] = (uchar) 'A';
-    s[4] = (uchar) 0;
-
-    float* fs = DynRankS::createCharDistribution(s, 5, 1);
-    dynsa::DynamicSuffixArray * a = new dynsa::DynamicSuffixArray(fs);
-    a->setText(s, 5);
-
-    cout << a->getText() << endl;
-    cout << a->getBWT() << endl;
-
-    a->deleteAt(1, 2);
-
-    cout << a->getText() << endl;
-    cout << a->getBWT() << endl;
-
-    a->deleteAt(1, 1);
-
-    cout << a->getText() << endl;
-    cout << a->getBWT() << endl;
-
-    return 0;
-}
-*/
-
-
 //Common variables for some tests
 dynsa::DynamicSuffixArray *a;
 ustring dist_string;
@@ -97,6 +67,18 @@ TEST_CASE("Short replace", "[short]"){
     
     ustring s = (ustring) "GTTTGTGAG";
     ustring bwt = (ustring) "GGATT$GTTG";
+    
+    int sz = strlen((char*)s);
+    CHECK((memcmp(a->getText(), s, sz) == 0));
+    sz = strlen((char*)bwt);
+    REQUIRE((memcmp(a->getBWT(), bwt, sz) == 0));
+}
+
+TEST_CASE("Single insert", "[short]"){
+    a -> insertToText((uchar)'C', 4);
+    cout << a -> getText() << endl << a -> getBWT() << endl;
+    ustring s = (ustring) "GTTCTGTGAG";
+    ustring bwt = (ustring) "GGTATT$TGCG";
     
     int sz = strlen((char*)s);
     CHECK((memcmp(a->getText(), s, sz) == 0));
